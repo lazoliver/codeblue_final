9804f2d0dea7b8744fad0bc2c20d5abb1b1f9a8a
lazoliver <wilian.oliveiractt@gmail.com>
2022-07-30 18:38:46 -0300

    see-more & see-less: function

--- js/script.js
+++ js/script.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
         $("nav").toggleClass("dropdown");
         $("#btn").toggleClass("btn-close")
     });
+    $("#see-more").click(function() {
+        $("#show-more").addClass("store-mobile");
+        $("#see-more").hide();
+        $("#see-less").show();
+    });
+    $("#see-less").click(function() {
+        $("#show-more").removeClass("store-mobile");
+        $("#see-more").show();
+        $("#see-less").hide();
+    });
 });
